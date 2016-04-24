935c903dc2fec87c2ee8a8730efc8c0746a507ed
Paul Ryan <paul@simplycomplex.co>
2016-04-24 13:19:07 -0600

    fix(org): Return types
    Return any from all for piping, would be WritableStream if not using through2.

--- index.d.ts
+++ index.d.ts
@@ -7,28 +7,28 @@ declare module "gulp-git" {
 
   namespace bump {
     interface Bump {
-      init(options?: Option, callback?: Function);
-      add(options?: Option): () => any;
-      commit(message: string|string[], options?: Option): () => any;
-      addRemote(remote: string, location: string, options?: Option, callback?: Function): () => any;
-      removeRemote(remote: string, options?: Option, callback?: Function): () => any;
-      push(remote: string, branch: string, options?: Option, callback?: Function): () => any;
-      pull(remote: string, branch: string, options?: Option, callback?: Function): () => any;
-      fetch(remote: string, branch: string, options?: Option, callback?: Function): () => any;
-      clone(url: string, options?: Option, callback?: Function): () => any;
-      tag(tag: string, message: string, options?: Option, callback?: Function): () => any;
-      branch(branch: string, options?: Option, callback?: Function): () => any;
-      checkout(branch: string, options?: Option, callback?: Function): () => any;
-      checkoutFiles(options?: Option): () => any;
-      merge(branch: string, options?: Option, callback?: Function): () => any;
-      reset(commit: string, options?: Option, callback?: Function): () => any;
-      revParse(options?: Option, callback?: Function): () => any;
-      rm(options?: Option): () => any;
-      addSubmodule(url: string, name: string, options?: Option): () => any;
-      updateSubmodule(options?: Option): () => any;
-      status(options?: Option, callback?: Function): () => any;
-      exec(options?: Option, callback?: Function): () => any;
-      clean(paths?: string, options?: Option, callback?: Function): () => any;
+      init(options?: Option, callback?: Function): any;
+      add(options?: Option): any;
+      commit(message: string|string[], options?: Option): any;
+      addRemote(remote: string, location: string, options?: Option, callback?: Function): any;
+      removeRemote(remote: string, options?: Option, callback?: Function): any;
+      push(remote: string, branch: string, options?: Option, callback?: Function): any;
+      pull(remote: string, branch: string, options?: Option, callback?: Function): any;
+      fetch(remote: string, branch: string, options?: Option, callback?: Function): any;
+      clone(url: string, options?: Option, callback?: Function): any;
+      tag(tag: string, message: string, options?: Option, callback?: Function): any;
+      branch(branch: string, options?: Option, callback?: Function): any;
+      checkout(branch: string, options?: Option, callback?: Function): any;
+      checkoutFiles(options?: Option): any;
+      merge(branch: string, options?: Option, callback?: Function): any;
+      reset(commit: string, options?: Option, callback?: Function): any;
+      revParse(options?: Option, callback?: Function): any;
+      rm(options?: Option): any;
+      addSubmodule(url: string, name: string, options?: Option): any;
+      updateSubmodule(options?: Option): any;
+      status(options?: Option, callback?: Function): any;
+      exec(options?: Option, callback?: Function): any;
+      clean(paths?: string, options?: Option, callback?: Function): any;
     }
     interface Option {
       cwd?: string,
